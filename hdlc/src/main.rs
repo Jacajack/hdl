c500becbc860533e5b5fd747da259196558a624b
Jacek Wieczorek <mrjjot@gmail.com>
2023-03-05 21:12:53 +0000

    Create dependency between `hdlc` and `hdllang`

--- hdlc/src/main.rs
+++ hdlc/src/main.rs
@@ -1,3 +1,7 @@
+extern crate hdllang;
+
 fn main() {
-    println!("Hello, world!");
+    let s = hdllang::say_something();
+    assert_eq!(s, "Hello, i am alive.");
+    println!("Hello, world! - {}", s);
 }
--- hdllang/src/lib.rs
+++ hdllang/src/lib.rs
@@ -2,6 +2,10 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
+pub fn say_something() -> String {
+    String::from("Hello, i am alive.")
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
