ea640f1bd76da6081ffa022185b2e44a7df4b915
Jacek Wieczorek <mrjjot@gmail.com>
2023-03-05 21:13:50 +0000

    Indent using tabs

--- hdlc/src/main.rs
+++ hdlc/src/main.rs
@@ -1,7 +1,7 @@
 extern crate hdllang;
 
 fn main() {
-    let s = hdllang::say_something();
-    assert_eq!(s, "Hello, i am alive.");
-    println!("Hello, world! - {}", s);
+	let s = hdllang::say_something();
+	assert_eq!(s, "Hello, i am alive.");
+	println!("Hello, world! - {}", s);
 }
--- hdllang/src/lib.rs
+++ hdllang/src/lib.rs
@@ -1,18 +1,18 @@
 pub fn add(left: usize, right: usize) -> usize {
-    left + right
+	left + right
 }
 
 pub fn say_something() -> String {
-    String::from("Hello, i am alive.")
+	String::from("Hello, i am alive.")
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+	#[test]
+	fn it_works() {
+		let result = add(2, 2);
+		assert_eq!(result, 4);
+	}
 }
