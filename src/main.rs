396ecb38c781ab9d24237388688c6243dcda9707
Jacek Wieczorek <mrjjot@gmail.com>
2023-03-07 20:14:02 +0100

    Restructure crates, improve VSCode integration

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,12 @@ use hdllang::lexer::Lexer;
 
 fn main() {
 	let mut lexer = LogosLexer::new();
-	match lexer.process("31 27 if > ; 44 /**/ */ /*12 34*/ 56 4457 11 24 /* if ; // 44 \n 11") {
+	let source = "31 for 27 test if > ; 44 /**/ */ /*12 34*/ 56 4457 11 24 /* if ; // 44 \n 11";
+	match lexer.process(source) {
 		Ok(tokens) => {
 			println!("okayy we have {} tokens", tokens.len());
 			for t in &tokens {
-				println!("Token {:?}", t.kind);
+				println!("Token {:?} - '{}'", t.kind, &source[t.range.start .. t.range.end]);
 			}
 		},
 		Err(_) => println!("lexer error!"),
