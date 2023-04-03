c012ea4481fc62766ba0c21069c1e5ad9db491f9
WojciechPtas <wojciech.ptas@gmail.com>
2023-04-03 12:11:15 +0200

    Added lalrpop macro to lib.rs

--- hdllang/src/lib.rs
+++ hdllang/src/lib.rs
@@ -2,6 +2,8 @@ pub mod lexer;
 pub mod compiler_diagnostic;
 pub mod compiler_error;
 pub mod source_span;
+#[macro_use] extern crate lalrpop_util;
+pub mod parser;
 
 pub use compiler_diagnostic::CompilerDiagnostic;
 pub use source_span::SourceSpan;
