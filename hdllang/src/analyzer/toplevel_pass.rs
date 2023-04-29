7304e947b3bf4362ee6c58e96e2dafe438abcfef
WojciechPtas <wojciech.ptas@gmail.com>
2023-04-29 23:31:25 +0200

    ignore non-necessary fields for this pass

--- hdllang/src/analyzer/toplevel_pass.rs
+++ hdllang/src/analyzer/toplevel_pass.rs
@@ -12,13 +12,11 @@ impl<'source> AnalyzerPass<'source> for ToplevelPass {
 			match def {
 				ModuleDeclaration {
 					id,
-					location: _,
-					statements: _,
+					..
 				} => debug!("Found module def for {:?}", ctx.id_table.get_by_key(id).unwrap()), // FIXME
 				ModuleImplementation {
 					id,
-					location: _,
-					statement: _,
+					..
 				} => debug!("Found module impl for {:?}", ctx.id_table.get_by_key(id).unwrap()), // FIXME
 			}
 		}
