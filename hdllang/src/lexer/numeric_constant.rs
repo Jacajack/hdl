b1aecf9a69dfe79b1863bc61f0c0814771bdb062
Jacek Wieczorek <mrjjot@gmail.com>
2023-05-23 17:55:13 +0200

    Implement `to_pretty_string()` for `NumericConstant`

--- hdllang/src/lexer/numeric_constant.rs
+++ hdllang/src/lexer/numeric_constant.rs
@@ -1,21 +1,55 @@
 use crate::core::WideUint;
 use serde::{Deserialize, Serialize};
 
+#[derive(Clone, Copy, Debug, Serialize, Deserialize, PartialEq, Eq)]
+pub enum NumericConstantBase {
+	Binary,
+	Decimal,
+	Hexadecimal,
+	Boolean,
+}
+
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct NumericConstant {
 	pub value: WideUint,
 	pub width: Option<u32>,
 	pub signed: Option<bool>,
+	pub base: Option<NumericConstantBase>,
 }
 
 impl NumericConstant {
 	/// Creates a new numeric constant from u64
-	pub fn from_u64(value: u64, width: Option<u32>, signed: Option<bool>) -> Self {
-		Self {
+	pub fn from_u64(value: u64, width: Option<u32>, signed: Option<bool>, base: Option<NumericConstantBase>) -> Self {
+		let num = Self {
 			value: WideUint::from_u64(value),
 			width,
 			signed,
+			base,
+		};
+		assert!(num.consistency_check());
+		num
+	}
+	
+	/// Internal consistency checks
+	fn consistency_check(&self) -> bool {
+		// Boolean constants
+		if matches!(self.base, Some(NumericConstantBase::Boolean)) {
+			if self.value != WideUint::from_u64(0) && self.value != WideUint::from_u64(1) {
+				return false;
+			}
 		}
+
+		// Id width is specified, signedness must be specified as well
+		if self.width.is_some() && self.signed.is_none() {
+			return false;
+		}
+
+		// Check if width is valid
+		if !self.is_width_valid() {
+			return false;
+		}
+
+		true
 	}
 
 	/// Returns true if the constant is fully constrained in terms
@@ -75,7 +109,46 @@ impl NumericConstant {
 			_ => None,
 		}
 	}
-	pub fn to_string(&self) -> String {
-		self.value.to_string()
+
+	/// Converts the numeric constant to a HIRL string
+	/// Used for pretty printer
+	pub fn to_pretty_string(&self) -> String {
+		assert!(self.consistency_check());
+		use NumericConstantBase::*;
+
+		// Special case for boolean constants
+		if matches!(self.base, Some(Boolean)) {
+			return if self.value.count_ones() == 0 {
+				"false".to_string()
+			} else {
+				"true".to_string()
+			}
+		}
+
+		let radix_prefix;
+		let radix;
+		match self.base {
+			Some(Binary) => {
+				radix_prefix = "0b";
+				radix = 2;
+			},
+			Some(Decimal) | None => {
+				radix_prefix = "";
+				radix = 10;
+			},
+			Some(Hexadecimal) => {
+				radix_prefix = "0x";
+				radix = 16;
+			},
+			Some(Boolean) => unreachable!(),
+		};
+
+		format!(
+			"{}{}{}{}",
+			radix_prefix,
+			self.value.to_string_radix(radix),
+			self.signed.map_or("", |s| if s { "s" } else { "u" }),
+			self.width.map_or("".to_string(), |w| w.to_string())
+		)
 	}
 }
