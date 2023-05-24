7fe4aefaa18d5d00daebd7a7296a8fec1da86841
Wojciech Ptaś <wojciech.ptas@gmail.com>
2023-05-24 21:19:17 +0200

    Update utils/parser_test.py
    Co-authored-by: Jacek Wieczorek <mrjjot@gmail.com>

--- utils/parser_test.py
+++ utils/parser_test.py
@@ -50,7 +50,7 @@ def log_unexpected(index:str):
 	if timeout:
 		unexp_or_timeout = "Timeout"
 	else:
-		unexp_or_timeout = "" if timeout else "Unexpected"
+		unexp_or_timeout = "Unexpected"
 	
 	# copy to failed-test dir with name <id>-unexpected.c or <id>-timeout.c
 	with open(TMP_TEST_PATH, "r") as fd_test:
