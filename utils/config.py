2b50410f09ac1a350b92eac84b620eb28e3dd6f2
WojciechPtas <wojciech.ptas@gmail.com>
2023-05-24 11:33:36 +0200

    Create Script for automatic fuzzing

---
+++ utils/config.py
@@ -0,0 +1,21 @@
+## Config
+
+## Constants and Paths
+BINARY_PATH = "target/debug/hdl"
+
+# Be verbose with command line outputs
+VERBOSE = False
+
+# Always abort after this amount of unexpected test results
+MAX_FAILED_TESTS = 30
+
+# Maximal numbers of non-terminals to expand
+# default is 15
+# Fuzzing will not work if this is too high
+MAX_NON_TERMINALS = 15
+
+# Minimal numbers of non-terminals to expand
+# default is 0
+MIN_NON_TERMINALS = 0
+
+
