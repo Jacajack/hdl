b7b70ac36e1bec69b92a8fa07fbf2dab75606a08
WojciechPtas <wojciech.ptas@gmail.com>
2023-06-14 23:40:08 +0200

    Change time of iterations with random seed to 5

--- utils/parser_test.py
+++ utils/parser_test.py
@@ -111,63 +111,68 @@ if __name__ == "__main__":
 	if args.seed:
 		random.seed(args.seed)
 		print(f"Starting fuzzing with seed = {args.seed}")
+		iterations = 1
 	else:
-		random.seed()
-		print(f"Starting fuzzing with random seed = {random.getstate()[1][0]}")
-	LOG_TO_FILE = args.log_to_file
-	if not os.path.exists("utils/tmp"):
-		os.makedirs("utils/tmp")
-	fuzzer = ProbabilisticGeneratorGrammarCoverageFuzzer(
-									GRAMMAR, 
-									min_nonterminals = MIN_NON_TERMINALS,
-									max_nonterminals = MAX_NON_TERMINALS,
-									log = False
-									)
+		iterations = 5
+	for i in range(iterations):
+		if iterations > 1:
+			random.seed()
+			print(f"Starting fuzzing with random seed = {random.getstate()[1][0]}")
+			print(f"Fuzzing iteration {i}")
+		LOG_TO_FILE = args.log_to_file
+		if not os.path.exists("utils/tmp"):
+			os.makedirs("utils/tmp")
+		fuzzer = ProbabilisticGeneratorGrammarCoverageFuzzer(
+										GRAMMAR, 
+										min_nonterminals = MIN_NON_TERMINALS,
+										max_nonterminals = MAX_NON_TERMINALS,
+										log = False
+										)
+
+		expected_total = 0
+		unexpected_total = 0
+		timeout_total = 0
+
+		i = 0
+		initilal_coverage = len(fuzzer.missing_expansion_coverage())
+		while len(fuzzer.missing_expansion_coverage()) >  0.05 * initilal_coverage:
+			i += 1
+
+			if unexpected_total >= MAX_FAILED_TESTS:
+				break
+
+			if VERBOSE:
+				print("Run:", i)
+				if i % 10 == 0:
+					print("Missing coverages:", fuzzer.missing_expansion_coverage())
+			elif i % 100 == 0:
+				print("Run:", i)
+				print("Amount of missing coverages:", len(fuzzer.missing_expansion_coverage()))
+
+			# redundant, to delete in next version
+			# Close file before running
+			with open(TMP_TEST_PATH, "w") as fd_test: 
+			# Redirect stdout to file for log
+				with open(TMP_LOG_PATH, "w") as sys.stdout:
+					fd_test.write(fuzzer.fuzz())
+
+			# Restore stdout
+			sys.stdout = sys.__stdout__
+
+			output, failed, timeout = run_test()
+			if not (failed or timeout):
+				expected_total += 1
+				log_expected(str(i))
+			else:
+				unexpected_total += 1
+				timeout_total += int(timeout)
+				log_unexpected(str(i))
+				print(f"Unexpected test: expected {expected_total}, unexpected {unexpected_total}, timeout {timeout_total}")
+
+		print(f"Done: expected {expected_total}, unexpected {unexpected_total}, timeout {timeout_total}")
+		print(f"Amount of missing coverages: {len(fuzzer.missing_expansion_coverage())} / {initilal_coverage}")
+		print(f"Missing coverages: {fuzzer.missing_expansion_coverage()}")
+		shutil.rmtree("utils/tmp")
 	
-	expected_total = 0
-	unexpected_total = 0
-	timeout_total = 0
-
-	i = 0
-	initilal_coverage = len(fuzzer.missing_expansion_coverage())
-	while len(fuzzer.missing_expansion_coverage()) >  0.05 * initilal_coverage:
-		i += 1
-
-		if unexpected_total >= MAX_FAILED_TESTS:
-			break
-
-		if VERBOSE:
-			print("Run:", i)
-			if i % 10 == 0:
-				print("Missing coverages:", fuzzer.missing_expansion_coverage())
-		elif i % 100 == 0:
-			print("Run:", i)
-			print("Amount of missing coverages:", len(fuzzer.missing_expansion_coverage()))
-
-		# redundant, to delete in next version
-		# Close file before running
-		with open(TMP_TEST_PATH, "w") as fd_test: 
-		# Redirect stdout to file for log
-			with open(TMP_LOG_PATH, "w") as sys.stdout:
-				fd_test.write(fuzzer.fuzz())
-		
-		# Restore stdout
-		sys.stdout = sys.__stdout__
-
-		output, failed, timeout = run_test()
-		if not (failed or timeout):
-			expected_total += 1
-			log_expected(str(i))
-		else:
-			unexpected_total += 1
-			timeout_total += int(timeout)
-			log_unexpected(str(i))
-			print(f"Unexpected test: expected {expected_total}, unexpected {unexpected_total}, timeout {timeout_total}")
-
-	print(f"Done: expected {expected_total}, unexpected {unexpected_total}, timeout {timeout_total}")
-	print(f"Amount of missing coverages: {len(fuzzer.missing_expansion_coverage())} / {initilal_coverage}")
-	print(f"Missing coverages: {fuzzer.missing_expansion_coverage()}")
-	shutil.rmtree("utils/tmp")
- 
-	if unexpected_total:
-		sys.exit(1)
+		if unexpected_total:
+			sys.exit(1)
