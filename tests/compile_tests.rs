f903c23a029c651a9f3ef11564174193ebc395f6
Jacek Wieczorek <mrjjot@gmail.com>
2023-10-17 15:59:16 +0200

    Implement `iverilog`-based codegen tests

--- tests/compile_tests.rs
+++ tests/compile_tests.rs
@@ -2,14 +2,14 @@ extern crate hdllang;
 use std::collections::HashMap;
 use std::io::Write;
 use std::path::{Path, PathBuf};
-use std::str;
-use std::fs::File;
 use hdllang::lexer::{IdTable, Lexer, LogosLexer, LogosLexerContext};
 use hdllang::parser::ast::Root;
 use hdllang::parser;
 use hdllang::parser::ParserError;
 use hdllang::compiler_diagnostic::ProvidesCompilerDiagnostic;
 use rstest::*;
+use tempfile::NamedTempFile;
+use subprocess::{Popen, PopenConfig, ExitStatus};
 
 // Copied from main.rs
 fn parse_file_recover_tables(
@@ -55,9 +55,9 @@ fn compile(mut code: String, file_name: String, output: &mut dyn Write) -> miett
 }
 
 
-fn run_hdlc(input_path: &Path) -> miette::Result<File> {
+fn run_hdlc(input_path: &Path) -> miette::Result<NamedTempFile> {
 	let src = std::fs::read_to_string(input_path).expect("failed to read source code");
-	let mut tmpfile: File = tempfile::tempfile().unwrap();
+	let mut tmpfile = NamedTempFile::new().unwrap();
 	compile(
 		src,
 		input_path.file_name().expect("filename needed").to_str().unwrap().into(),
@@ -65,10 +65,39 @@ fn run_hdlc(input_path: &Path) -> miette::Result<File> {
 	Ok(tmpfile)
 }
 
+fn run_iverilog(iverilog_path: &Path, input_path: &Path) -> Result<NamedTempFile, String> {
+	let bin_file = NamedTempFile::new().unwrap();
+
+	let mut p = Popen::create(
+		&[
+			iverilog_path.to_str().unwrap(),
+			"-g2005-sv",
+			input_path.to_str().unwrap(),
+			"-o", bin_file.path().to_str().unwrap()
+		],
+		PopenConfig::default(),
+	).expect("failed to spawn iverilog");
+
+	use ExitStatus::*;
+	match p.wait().unwrap() {
+		Exited(0) => {},
+		Exited(err) => return Err(format!("Exit code: {}", err)),
+		Signaled(signum) => return Err(format!("Signaled: {}", signum)),
+		Other(err) => return Err(format!("Other: {}", err)),
+		Undetermined => panic!("cannot determine exit code"),
+	}
+
+	Ok(bin_file)
+}
 
 #[rstest]
 fn test_compile_success(#[files("tests/input/*.hirl")] path: PathBuf) {
-	run_hdlc(path.as_path()).unwrap();	
+	let sv_file = run_hdlc(path.as_path()).unwrap();
+	
+	if !std::env::var("NO_IVERILOG").is_ok() {
+		let iverilog_path = std::env::var("IVERILOG_PATH").unwrap_or("iverilog".into());
+		let _bin_file = run_iverilog(Path::new(&iverilog_path), sv_file.path()).unwrap();
+	}
 }
 
 #[rstest]
