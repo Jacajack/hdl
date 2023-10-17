d4ab6b0df04e3fa3caa8e9a6878f7ffef1ef6aaf
Jacek Wieczorek <mrjjot@gmail.com>
2023-10-17 16:21:37 +0200

    Enable all `iverilog` warnings

--- tests/compile_tests.rs
+++ tests/compile_tests.rs
@@ -72,6 +72,7 @@ fn run_iverilog(iverilog_path: &Path, input_path: &Path) -> Result<NamedTempFile
 		&[
 			iverilog_path.to_str().unwrap(),
 			"-g2005-sv",
+			"-Wall",
 			input_path.to_str().unwrap(),
 			"-o", bin_file.path().to_str().unwrap()
 		],
