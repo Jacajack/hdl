3ec6fca2fca44d55884655d7a8d4f95da1a33eb9
WojciechPtas <wojciech.ptas@gmail.com>
2023-11-01 21:24:29 +0100

    Add tests for pretty-print-switch

--- hdllang/src/parser/ast/variable_definition/pretty_printable.rs
+++ hdllang/src/parser/ast/variable_definition/pretty_printable.rs
@@ -4,8 +4,11 @@ use crate::parser::pretty_printer::*;
 impl PrettyPrintable for VariableDefinition {
 	fn pretty_print(&self, ctx: &mut PrettyPrinterContext) -> miette::Result<()> {
 		self.type_declarator.pretty_print(ctx)?;
-		for direct in &self.initializer_list {
-			direct.pretty_print(ctx)?;
+		for i  in 0..self.initializer_list.len() {
+			self.initializer_list[i].pretty_print(ctx)?;
+			if i != self.initializer_list.len()- 1 {
+				ctx.write( ", ")?;
+			}
 		}
 		Ok(())
 	}
