919da5336bdd2fbd538dd0d0df6de98aa46f86bd
Jacek Wieczorek <mrjjot@gmail.com>
2023-11-14 13:49:21 +0000

    Revert SV codegen workaround

--- hirn/src/codegen/sv/sv_codegen.rs
+++ hirn/src/codegen/sv/sv_codegen.rs
@@ -215,15 +215,9 @@ impl<'a> SVCodegen<'a> {
 	}
 
 	fn format_module_parameter_declaration(&mut self, s: InterfaceSignal) -> Result<String, CodegenError> {
-		Ok(format!(
-			"parameter {}",
-			self.translate_expression(&s.signal.into(), false)?
-		))
-		
-		// FIXME this code should be in use! However our CI is running old iverilog version
-		// and it needs to be updated, because it doesn't support this syntax
-		// let sig = self.design.get_signal(s.signal).unwrap();
-		// self.format_param_declaration_impl(sig.name(), &sig.class, &sig.dimensions, &None, false)
+		// TODO Note: we could fall back to plain 'parameter' here for max compatibility
+		let sig = self.design.get_signal(s.signal).unwrap();
+		self.format_param_declaration_impl(sig.name(), &sig.class, &sig.dimensions, &None, false)
 	}
 
 	fn emit_assignment(&mut self, lhs: &Expression, rhs: &Expression) -> Result<(), CodegenError> {
