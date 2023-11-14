9a95ad276de2b1292fe453e533e7856ea1e1f38c
Jacek Wieczorek <mrjjot@gmail.com>
2023-11-14 17:44:32 +0100

    Split `signal_graph_pass` into more modules

---
+++ hirn/src/elab/multi_pass_elab/signal_graph_pass/scope_pass.rs
@@ -0,0 +1,55 @@
+use crate::design::ScopeId;
+
+use super::{SignalGraphPassCtx, ScopePassInfo};
+
+/// IDs generated each time a scope is visited
+#[derive(Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Clone)]
+pub struct ScopePassId(usize);
+
+impl SignalGraphPassCtx {
+	/// Records that an unconditional scope has been visited
+	pub(super) fn record_scope_pass(&mut self, id: ScopeId) -> ScopePassId {
+		let handle = self.design.get_scope_handle(id).expect("scope not in design");
+		let pass_id = ScopePassId(self.scope_pass_counter);
+		self.scope_pass_counter += 1;
+		self.pass_info.insert(pass_id, ScopePassInfo::Unconditional{
+			id,
+			parent_id: handle.parent()
+		});
+		self.current_pass.insert(id, pass_id);
+		pass_id
+	}
+
+	/// Records that a conditional scope has been visited
+	pub(super) fn record_conditional_scope_pass(&mut self, id: ScopeId, cond_true: bool) -> ScopePassId {
+		let handle = self.design.get_scope_handle(id).expect("scope not in design");
+		let pass_id = ScopePassId(self.scope_pass_counter);
+		self.scope_pass_counter += 1;
+		self.pass_info.insert(pass_id, ScopePassInfo::Conditional {
+			id,
+			parent_id: handle.parent().expect("conditional scope has no parent"),
+			was_true: cond_true,
+		});
+		self.current_pass.insert(id, pass_id);
+		pass_id
+	}
+
+	/// Recrods a pass through a range scope
+	pub(super) fn record_range_scope_pass(&mut self, id: ScopeId, iter: i64) -> ScopePassId {
+		let handle = self.design.get_scope_handle(id).expect("scope not in design");
+		let pass_id = ScopePassId(self.scope_pass_counter);
+		self.scope_pass_counter += 1;
+		self.pass_info.insert(pass_id, ScopePassInfo::Range {
+			id,
+			parent_id: handle.parent().expect("range scope has no parent"),
+			iter_number: iter,
+		});
+		self.current_pass.insert(id, pass_id);
+		pass_id
+	}
+
+	/// Returns current pass ID for a scope
+	pub fn get_scope_pass_id(&self, id: ScopeId) -> ScopePassId {
+		self.current_pass.get(&id).copied().expect("scope pass not recorded!")
+	}
+}
---
+++ hirn/src/elab/multi_pass_elab/signal_graph_pass/signal_drive.rs
@@ -0,0 +1,96 @@
+use std::sync::Arc;
+
+use log::{error, debug};
+
+use crate::{design::{SignalSliceRange, Evaluates, SignalId}, elab::{ElabAssumptionsBase, ElabMessageKind, multi_pass_elab::signal_graph_pass::GeneratedSignalRef, ElabSignal}};
+
+use super::SignalGraphPassCtx;
+struct SignalSliceRangeEvalResult<'a> {
+	elab_sig: &'a mut ElabSignal,
+	lsb_msb: Option<(i64, i64)>,
+}
+
+impl SignalGraphPassCtx {
+	fn eval_slice_range(&mut self, range: SignalSliceRange, assumptions: Arc<dyn ElabAssumptionsBase>) -> Result<SignalSliceRangeEvalResult, ElabMessageKind> {
+		let sig_ref = self.get_generated_signal_ref(range.slice(), assumptions.clone())?;
+		let elab_sig = self.elab_signals.get_mut(&sig_ref).expect("Sliced signal not registered in elab");
+		
+		if range.is_full() {
+			Ok(SignalSliceRangeEvalResult {
+				elab_sig,
+				lsb_msb: None,
+			})
+		} else {
+			let lsb_expr = range.lsb().unwrap();
+			let msb_expr = range.msb().unwrap();
+			let lsb_val = lsb_expr.eval(&assumptions)?;
+			let msb_val = msb_expr.eval(&assumptions)?;
+			let lsb = lsb_val.try_into_i64()?;
+			let msb = msb_val.try_into_i64()?;
+
+			if lsb < 0 || msb < 0 || msb < lsb || msb >= (elab_sig.width() as i64) {
+				error!("Signal {:?} (width {}) is driven with invalid range {}:{}", range.signal(), elab_sig.width(), msb, lsb);
+				return Err(ElabMessageKind::InvalidSignalBitRange);
+			}
+
+			Ok(SignalSliceRangeEvalResult{
+				elab_sig,
+				lsb_msb: Some((lsb, msb)),
+			})
+		}
+	}
+
+	pub fn drive_signal(&mut self, range: &SignalSliceRange, assumptions: Arc<dyn ElabAssumptionsBase>) -> Result<(), ElabMessageKind> {
+		debug!("Driving signal {:?}", range);
+		
+		let result = self.eval_slice_range(range.clone(), assumptions)?;		
+		if let Some((lsb, msb)) = result.lsb_msb {
+			result.elab_sig.drive_bits(lsb as u32, msb as u32);
+		} else {
+			result.elab_sig.drive();
+		}
+
+		Ok(())
+	}
+
+	pub fn read_signal(&mut self, range: &SignalSliceRange, assumptions: Arc<dyn ElabAssumptionsBase>) -> Result<(), ElabMessageKind> {
+		debug!("Marking signal as read {:?}", range);
+		
+		let result = self.eval_slice_range(range.clone(), assumptions)?;		
+		if let Some((lsb, msb)) = result.lsb_msb {
+			result.elab_sig.read_bits(lsb as u32, msb as u32);
+		} else {
+			result.elab_sig.read();
+		}
+
+		Ok(())
+	}
+
+	pub fn drive_array(&mut self, id: SignalId) -> Result<(), ElabMessageKind> {
+		debug!("Driving full array signal {:?}", id);
+		let gen_id = self.get_generated_signal_id(id);
+		let gen_sig = self.get_generated_signal(&gen_id);
+
+		for i in 0..gen_sig.total_fields() {
+			let sig_ref = GeneratedSignalRef::new(gen_id, Some(i as u32));
+			let elab_sig = self.elab_signals.get_mut(&sig_ref).expect("Sliced signal not registered in elab");
+			elab_sig.drive();
+		}
+
+		Ok(())
+	}
+
+	pub fn read_array(&mut self, id: SignalId) -> Result<(), ElabMessageKind> {
+		debug!("Driving full array signal {:?}", id);
+		let gen_id = self.get_generated_signal_id(id);
+		let gen_sig = self.get_generated_signal(&gen_id);
+
+		for i in 0..gen_sig.total_fields() {
+			let sig_ref = GeneratedSignalRef::new(gen_id, Some(i as u32));
+			let elab_sig = self.elab_signals.get_mut(&sig_ref).expect("Sliced signal not registered in elab");
+			elab_sig.read();
+		}
+
+		Ok(())
+	}
+}
